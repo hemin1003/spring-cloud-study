a3c0862e7d5c8cb048c24d03684c0a842f778158
minbohe <hemin_it@163.com>
2019-08-05 17:42:15 +0800

    升级到SpringCloud的Finchley版本(SpringBoot2.x版本)

--- microservice-config-client-eureka/src/main/java/com/minbo/cloud/study/controller/ConfigClientController.java
+++ microservice-config-client-eureka/src/main/java/com/minbo/cloud/study/controller/ConfigClientController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 这边的@RefreshScope注解不能少，否则即使调用/refresh，配置也不会刷新
+ * 刷新接口：http://localhost:8051/actuator/refresh
  */
 @RestController
 @RefreshScope
 public class ConfigClientController {
-  @Value("${profile}")
-  private String profile;
 
-  @GetMapping("/hello")
-  public String hello() {
-    return this.profile;
-  }
+	@Value("${profile}")
+	private String profile;
+
+	@GetMapping("/hello")
+	public String hello() {
+		return this.profile;
+	}
 }
