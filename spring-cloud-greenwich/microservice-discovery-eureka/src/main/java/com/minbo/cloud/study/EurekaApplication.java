b43f143bbd0777502af28cbd506bd29a353ea097
qq1187701489 <1187701489@qq.com>
2020-10-21 16:58:22 +0800

    测试提交git

--- spring-cloud-greenwich/microservice-discovery-eureka/src/main/java/com/minbo/cloud/study/EurekaApplication.java
+++ spring-cloud-greenwich/microservice-discovery-eureka/src/main/java/com/minbo/cloud/study/EurekaApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
+	//测试提交git
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
